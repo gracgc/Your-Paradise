8ef7bda47f8905c52d418f1c03d96dd1f51250f6
gracgc <61702739+gracgc@users.noreply.github.com>
2020-08-27 19:37:30 +0300

    Updates

---
+++ precache-manifest.3956a4d80846905de89208819083e61f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021b3228c615c445bcebd799a1dd7e34",
+    "url": "/Your-Paradise/index.html"
+  },
+  {
+    "revision": "962067460ad24ae93fc7",
+    "url": "/Your-Paradise/static/css/main.7f4e775e.chunk.css"
+  },
+  {
+    "revision": "447bcd22ed3efafc3037",
+    "url": "/Your-Paradise/static/js/2.c127e0d7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Your-Paradise/static/js/2.c127e0d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962067460ad24ae93fc7",
+    "url": "/Your-Paradise/static/js/main.c1844369.chunk.js"
+  },
+  {
+    "revision": "eac65c96483e7754c6ff",
+    "url": "/Your-Paradise/static/js/runtime-main.6f31a985.js"
+  },
+  {
+    "revision": "1020fd8e7aa4e46f94a3d5036b390bfc",
+    "url": "/Your-Paradise/static/media/Dual Ring-1s-200px.1020fd8e.gif"
+  }
+]);
